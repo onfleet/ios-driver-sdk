5b48ce77e37a3eb55cff798c1f7914a73c1e2227
Peter Stajger <stajger.p@gmail.com>
2021-01-13 17:43:38 +0100

    added Sample App to git repository

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0' 
+use_frameworks! 
+    
+target 'SampleApp' do 
+  pod 'OnfleetDriver', :git => 'https://github.com/onfleet/ios-driver-sdk.git' 
+end
---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Peter Stajger on 17/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
